7cd7d41337d71fe6a2187b256e65ceb2203beb64
bogusbd <bogusbd@gmail.com>
2017-10-09 03:53:37 +0300

    Wrong path..

--- JqueryCookieAsset.php
+++ JqueryCookieAsset.php
@@ -4,7 +4,7 @@ namespace uran1980\yii\assets\jQueryEssential;
 
 class JqueryCookieAsset extends \yii\web\AssetBundle
 {
-    public $sourcePath = '@bower/jquery-cookie';
+    public $sourcePath = '@bower/jquery-cookie/src/';
     public $js = [
         'jquery.cookie.js',
     ];
